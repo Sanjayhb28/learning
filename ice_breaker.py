9448ed4c415ab6bf1c0c83963c69efccc170d3ae
Sanjay <sanjayhb3@gmail.com>
2025-08-11 18:09:50 +0530

    add logic scrap data

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,16 +2,16 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 import os
 
-information = """ Elon Reeve Musk FRS is a businessman, known for his leadership of Tesla, SpaceX, X (formerly Twitter), and the Department of Government Efficiency (DOGE). Musk has been the wealthiest person in the world since 2021; as of May 2025, Forbes estimates his net worth to be US$424.7 billion.
+from third_parties.linkedin import scrape_linkedin_profile
 
-Born to a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada. He received bachelor's degrees from the University of Pennsylvania in 1997 before moving to California, United States, to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen."""
 
 if __name__ == "__main__":
     load_dotenv()
 
-    summary_template = """Given he information {information} abtout a person I want you to create, 
+    summary_template = """Given the linkedin information {information} abtout a person I want you to create, 
     1. A short summary of the person.
     2. Two interseting facts about them"""
 
@@ -25,12 +25,15 @@ if __name__ == "__main__":
         max_tokens=None,
         timeout=None,
         max_retries=2,
+        langsmit_tracing = False
         # other params...
     )
 
     chain = summary_prompt_template | llm
+    url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/5eaf8e46dc29a98612c8fe0c774123a7a2ac4575/eden-marco-scrapin.json"
+    linkedin_data = scrape_linkedin_profile(url, mock=True)
 
-    ai_msg = chain.invoke(input={"information": information})
+    ai_msg = chain.invoke(input={"information": linkedin_data})
     print(ai_msg)
 
     # print(ai_msg.content)
